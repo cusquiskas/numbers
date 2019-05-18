1e433511f9b9f0c7b432ff9c085244242ded9568
cusquiskas <cusquiskas@gmail.com>
2019-05-18 17:05:10 +0200

    creación del index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Otro proyecto de CusQuisKas & Cyberalmirante</title>
+    </head>
+    <body>
+        <template id="cabecera"></template>
+        <template id="cuerpo"></template>
+        <template id="pie"></template>
+    </body>
+</html>
